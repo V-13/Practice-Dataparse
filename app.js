af818dd3679d63d68c4d8ad8117a3b8ff30276c4
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-19 22:36:52 +0530

    data parse

---
+++ app.js
@@ -0,0 +1,27 @@
+const Express =require('express');
+var app= new Express();
+
+var bodyParser=require('body-parser');
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(rq,rs)=>{
+    rs.render('info',{title:"Student info"});
+});
+
+app.post('/read',(rq,rs)=>{
+    var x=rq.body
+    rs.render('read',{info:x,title:'values'});
+    var array=rq.body
+    console.log(array);
+});
+app.get('/info',(rq,rs)=>{
+    rs.render('info',{title:'Student info'});
+});
+
+app.listen(5000,()=>{
+    console.log("server running on 5000");
+});
